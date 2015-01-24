297e5285802dee1c52dbb8e36098c3b4ed7a6853
Nidedaye <jonathan.nijingqiao@gmail.com>
2015-01-24 13:59:06 -0700

    add UML add class Item and Claim

---
+++ src/ca/ualberta/cs/moneytrack/Claim.java
@@ -0,0 +1,5 @@
+package ca.ualberta.cs.moneytrack;
+
+public class Claim {
+
+}
---
+++ src/ca/ualberta/cs/moneytrack/Item.java
@@ -0,0 +1,16 @@
+package ca.ualberta.cs.moneytrack;
+
+public class Item {
+	protected String itemName;
+	public Item(String itemName) {
+		// TODO Auto-generated constructor stub
+		this.itemName=itemName;
+	}
+
+	public String getItemName() {
+		// TODO Auto-generated method stub
+		
+		return this.itemName;
+	}
+
+}
