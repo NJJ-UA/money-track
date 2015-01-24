d7068ed41fe84983ba55969a2e8abe6a8eaac29b
Nidedaye <jonathan.nijingqiao@gmail.com>
2015-01-24 15:28:35 -0700

    Finish draft test for class item

--- src/ca/ualberta/cs/moneytrack/test/ItemTest.java
+++ src/ca/ualberta/cs/moneytrack/test/ItemTest.java
@@ -5,8 +5,31 @@ import ca.ualberta.cs.moneytrack.Item;
 
 public class ItemTest extends TestCase {
 	public void testItem(){
+		//test construct with name and getItemName()
 		String itemName="KTV SJW";
 		Item item =new Item(itemName);
 		assertTrue("Item Name is not Equal", itemName.equals(item.getItemName()));
+		//test addItemDate() and getItemDate()
+		int itemDate=20141222;
+		item.addItemDate(itemDate);
+		assertTrue("Item date is not equal",itemDate==item.getItemDate());
+		//test addCategory() and getCategory()
+		String category="YuLe";
+		item.addCategory(category);
+		assertTrue("Category is not equal",category==item.getCategory());
+		//test addDescription() and getDescription()
+		String description ="This KTV is good";
+		item.addDescription(description);
+		assertTrue("description is not equal",description==item.getDescription());
+		//test addAmount() and getAmount()
+		int amount=40;
+		item.addAmount(amount);
+		assertTrue("amount is not equal",amount==item.getAmount());
+		//test addCurrency() and getCurrency()
+		String currency="CAD";
+		item.addCurrency(currency);
+		assertTrue("currency is not equal",currency==item.getCurrency());
 	}
+	
+	
 }
